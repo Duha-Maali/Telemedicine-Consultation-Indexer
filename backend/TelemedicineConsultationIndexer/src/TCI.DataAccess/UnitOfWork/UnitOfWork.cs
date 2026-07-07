fa401d33bb4c667cfdbf62b712d6f564e569c136
Duha Maali <duhamaali8@gmail.com>
2026-07-08 01:17:04 +0300

    Add logging to unit of work

--- backend/TelemedicineConsultationIndexer/src/TCI.DataAccess/UnitOfWork/UnitOfWork.cs
+++ backend/TelemedicineConsultationIndexer/src/TCI.DataAccess/UnitOfWork/UnitOfWork.cs
@@ -1,12 +1,32 @@
-﻿using TCI.DataAccess.Persistence;
+﻿using Microsoft.Extensions.Logging;
+using TCI.DataAccess.Persistence;
 
 namespace TCI.DataAccess.UnitOfWork;
 
-public class UnitOfWork(AppDbContext context) : IUnitOfWork
+public class UnitOfWork(
+    AppDbContext context,
+    ILogger<UnitOfWork> logger) : IUnitOfWork
 {
     private readonly AppDbContext _context = context;
+
+    private readonly ILogger<UnitOfWork> _logger = logger;
     public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
     {
-        await _context.SaveChangesAsync(cancellationToken);
+        try
+        {
+            var affectedRows = await _context.SaveChangesAsync(cancellationToken);
+
+            _logger.LogDebug(
+                   "Database changes saved successfully. AffectedRows={AffectedRows}.",
+                   affectedRows);
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(
+                exception,
+                "Failed to save database changes.");
+
+            throw;
+        }
     }
 }
